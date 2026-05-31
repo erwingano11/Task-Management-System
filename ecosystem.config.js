65b6aee16e6c999ebbe857678b426eade6b1360e
erwingano18-cyber <erwingano18@gmail.com>
2026-05-31 14:19:37 +0800

    deployment

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [
+    {
+      name: "wms-backend",
+      script: "server.js",
+      cwd: "/var/www/wms/backend",
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "300M",
+      env_production: {
+        NODE_ENV: "production",
+        PORT: 5000,
+      },
+    },
+  ],
+};
